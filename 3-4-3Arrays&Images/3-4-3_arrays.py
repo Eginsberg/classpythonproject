9adb5f31d74fd1be3bd0b9585cfd2095f81acf51
GINSBERG ETHAN M <egin1901@mpsedu.org>
2018-05-08 08:52:36 -0500

    Thanos in fortnite XD

--- 3-4-3Arrays&Images/3-4-3_arrays.py
+++ 3-4-3Arrays&Images/3-4-3_arrays.py
@@ -1,4 +1,7 @@
-img = tinyCat.jpg
-type(img)
-img
-len(img)
+import os.path
+import matplotlib.pyplot as plt
+
+directory = os.path.dirname(os.path.abspath(__file__)) 
+filename = os.path.join(directory, 'tinyCat.jpg')
+
+img = plt.imread(filename)
--- pythonreferance.py
+++ pythonreferance.py
@@ -1,10 +1,19 @@
+'''Acesses file in the current directory'''
+import os.path
+
 # Get the directory of this python script
 directory = os.path.dirname(os.path.abspath(__file__)) 
 # Build an absolute filename from directory + filename
 filename = os.path.join(directory, 'woman.jpg')
+
+'''Acesses image'''
+import matplotlib.pyplot as plt 
+
 # Read the image data into an array
 img = plt.imread(filename)
 
+
+'''Displays image'''
 # Create figure with 2 subplots
 fig, ax = plt.subplots(1, 3)
 # Show the image data in the first subplot
@@ -12,3 +21,9 @@ ax[0].imshow(img, interpolation='none')
 
 # Show the figure on the screen
 fig.show()
+
+
+
+
+
+
