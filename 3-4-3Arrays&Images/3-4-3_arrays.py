6c1b6e6e7e2c621c54a82dc3d949b32efb254501
GINSBERG ETHAN M <egin1901@mpsedu.org>
2018-05-10 10:14:14 -0500

    More Updates

--- 3-4-3Arrays&Images/3-4-3_arrays.py
+++ 3-4-3Arrays&Images/3-4-3_arrays.py
@@ -2,6 +2,6 @@ import os.path
 import matplotlib.pyplot as plt
 
 directory = os.path.dirname(os.path.abspath(__file__)) 
-filename = os.path.join(directory, 'tinyCat.jpg')
+filename = os.path.join(directory, 'cat1-a.gif')
 
 img = plt.imread(filename)
